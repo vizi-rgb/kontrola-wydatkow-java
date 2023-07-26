a0b0b14138eb5d0f2fa595632f48cc781c919b88
Wiktor Nasierowski <wiktor.nasierowski@gmail.com>
2023-07-26 23:33:35 +0200

    Added tests

--- src/main/java/org/javatest/command/CommandBuilder.java
+++ src/main/java/org/javatest/command/CommandBuilder.java
@@ -1,6 +1,7 @@
 package org.javatest.command;
 
 import lombok.Getter;
+import lombok.NonNull;
 
 @Getter
 public class CommandBuilder {
@@ -9,17 +10,25 @@ public class CommandBuilder {
     private String description;
     private Handler handler;
 
-    public CommandBuilder withActuator(String actuator) {
+    public CommandBuilder withActuator(@NonNull String actuator) {
+        if (actuator.isBlank()) {
+            throw new IllegalArgumentException("Actuator cannot be a blank string");
+        }
+
         this.actuator = actuator;
         return this;
     }
 
     public CommandBuilder withDescription(String description) {
+        if (description.isBlank()) {
+            description = "Description not provided";
+        }
+
         this.description = description;
         return this;
     }
 
-    public CommandBuilder withHandler(Handler handler) {
+    public CommandBuilder withHandler(@NonNull Handler handler) {
         this.handler = handler;
         return this;
     }
--- src/test/java/CommandBuilderTest.java
+++ src/test/java/CommandBuilderTest.java
@@ -1,3 +1,85 @@
+import org.javatest.command.Command;
+import org.javatest.command.CommandBuilder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.javatest.command.Handler;
+
 public class CommandBuilderTest {
 
+    @Test
+    public void should_ThrowNullPointerException_WhenPassedNullActuator() {
+        // given
+        CommandBuilder commandBuilder = new CommandBuilder();
+        String nullActuator = null;
+
+        // when
+
+        // then
+        Assertions.assertThrows(
+                NullPointerException.class,
+                () -> commandBuilder.withActuator(nullActuator));
+    }
+
+    @Test
+    public void should_ThrowIllegalArgumentException_WhenPassedBlankActuator() {
+        // given
+        CommandBuilder commandBuilder = new CommandBuilder();
+        String emptyActuator = "";
+        String whitespaceActuator = " \n\t ";
+
+        // when
+
+        // then
+        Assertions.assertAll(
+                () -> Assertions.assertThrows(
+                        IllegalArgumentException.class,
+                        () -> commandBuilder.withActuator(emptyActuator)
+                ),
+
+                () -> Assertions.assertThrows(
+                       IllegalArgumentException.class,
+                        () -> commandBuilder.withActuator(whitespaceActuator)
+                )
+        );
+    }
+
+    @Test
+    public void should_ThrowNullPointerException_WhenPassedNullHandler() {
+        // given
+        CommandBuilder commandBuilder = new CommandBuilder();
+        Handler nullHandler = null;
+
+        // when
+
+        // then
+        Assertions.assertThrows(
+                NullPointerException.class,
+                () -> commandBuilder.withHandler(nullHandler));
+    }
+
+    @Test
+    public void should_BuildCommand_WhenPassedGoodParams() {
+        // given
+        CommandBuilder commandBuilder = new CommandBuilder();
+        Handler handler = new Handler() {
+            @Override
+            public void handle() {
+                return;
+            }
+        };
+
+        // when
+        Command newCommand = commandBuilder
+                .withActuator("test")
+                .withDescription("No description")
+                .withHandler(handler)
+                .build();
+
+        // then
+        Assertions.assertAll(
+                () -> Assertions.assertEquals("test", newCommand.getActuator()),
+                () -> Assertions.assertEquals("No description", newCommand.getDescription()),
+                () -> Assertions.assertEquals(handler, newCommand.getHandler())
+        );
+    }
 }
