871764c79edf622bb21ddd5bd9cfad96af177cdf
Wiktor Nasierowski <wiktor.nasierowski@gmail.com>
2023-08-20 15:10:27 +0200

    Tests added

--- src/main/java/org/javatest/command/MyDateFormat.java
+++ src/main/java/org/javatest/command/MyDateFormat.java
@@ -2,6 +2,16 @@ package org.javatest.command;
 
 public record MyDateFormat(int day, int month, int year) {
 
+    public MyDateFormat {
+        if (day < 1 || day > 31) {
+            throw new IllegalArgumentException("Day must be in <1, 31> range");
+        }
+
+        if (month < 1 || month > 12) {
+            throw new IllegalArgumentException("Month must be in <1, 12> range");
+        }
+    }
+
     @Override
     public String toString() {
        return padIfNeeded(Integer.toString(day)) + "." +
